e06a0a749962a218428450da140bea9f61afc352
JAPACHECOV <japachecov@gmail.com>
2022-02-08 16:09:25 -0600

    segunda actualizacion

--- scripts/TEST/00-test.R
+++ scripts/TEST/00-test.R
@@ -1,3 +1,24 @@
 # El script permite guardar instrucciones como alternativa a sesiones o archivos de historial. Se pueden comentar cosas, instrucciones, fallos etc.
 # Se pueden comentar cosas que no han funcionado en vez de borrarlas
 # LOs comentarios se inician con un simblo #
+
+require(graphics)
+
+## Annette Dobson (1990) "An Introduction to Generalized Linear Models".
+## Page 9: Plant Weight Data.
+ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
+trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
+group <- gl(2, 10, 20, labels = c("Ctl","Trt"))
+weight <- c(ctl, trt)
+lm.D9 <- lm(weight ~ group)
+lm.D90 <- lm(weight ~ group - 1) # omitting intercept
+
+anova(lm.D9)
+summary(lm.D90)
+
+opar <- par(mfrow = c(2,2), oma = c(0, 0, 1.1, 0))
+plot(lm.D9, las = 1)      # Residuals, Fitted, ...
+par(opar)
+
+### less simple examples in "See Also" above
+
