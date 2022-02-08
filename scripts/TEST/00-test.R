bed5d04752fbc99bb584f96ae7b49250f7fa39bc
JAPACHECOV <japachecov@gmail.com>
2022-02-08 15:53:59 -0600

    1 er dia de clases

---
+++ scripts/TEST/00-test.R
@@ -0,0 +1,3 @@
+# El script permite guardar instrucciones como alternativa a sesiones o archivos de historial. Se pueden comentar cosas, instrucciones, fallos etc.
+# Se pueden comentar cosas que no han funcionado en vez de borrarlas
+# LOs comentarios se inician con un simblo #
