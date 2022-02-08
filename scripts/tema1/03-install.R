cdbdaa9c108b6f07fd4e209d35713d69ca3463bf
JAPACHECOV <japachecov@gmail.com>
2022-02-08 16:29:58 -0600

    3a actualizacion

--- scripts/tema1/03-install.R
+++ scripts/tema1/03-install.R
@@ -1,11 +1,14 @@
-install.packages("tidyverse", dep = TRUE)
+install.packages("tidyverse", dep = TRUE) # comando Para instalar paquetes externos. dependencias True instala todas las dependencias.
 
-library(tidyverse)
+library(tidyverse) # Cargar la libreria del paquete, se puede activar tambien desde la pestaña packages
 
 install.packages("magic", dep = TRUE)
 
 library(magic)
 
-magic(6)
+magic(6) # comando del paquete Magic
 
-installed.packages()
+installed.packages() # ver que librerias estan isntaladas
+
+# Cuando cierra s sesion se descargan algunas librerias
+# Se pueden cargar librerias a traves de install en la pestaña de packages
